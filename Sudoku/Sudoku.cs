b64f8d34c7247502a5bf1909b946d355f30478e9
richard davy <richard_davy@hotmail.com>
2018-09-12 09:51:20 +0200

    Snyggat till utskriften.

--- Sudoku/Sudoku.cs
+++ Sudoku/Sudoku.cs
@@ -22,25 +22,29 @@ namespace Sudoku
         public string FormatString (string initArragement)
         {
             char[,] matrix = new char[9, 9];
-            string formatedMatrix = "+-----------+-----------+-----------+\n";
+            string formatedMatrix = "+---------+---------+---------+\n";
 
             for (int i = 0; i < WidthOfBoard; i++)
             {
                 for (int j = 0; j < WidthOfBoard; j++)
                 {
-                    if (initArragement[j]==0)
+                    if (j%3==0)
+                    {
+                        formatedMatrix += "|";
+                    }
+                    if (initArragement[j + (i * 9)]=='0')
                     {
                         matrix[i, j] = ' ';
                     } else
                     {
-                        matrix[i, j] = initArragement[j];
+                        matrix[i, j] = initArragement[j + (i * 9)];
                     }                    
-                    formatedMatrix += String.Format("| {0} ", matrix[i, j]);
+                    formatedMatrix += String.Format(" {0} ", matrix[i, j]);
                 }
                 formatedMatrix += "|\n";
                 if ((i+1) % 3 == 0)
                 {
-                    formatedMatrix += "+-----------+-----------+-----------+\n";
+                    formatedMatrix += "+---------+---------+---------+\n";
                 }
             }
             //formatedMatrix += "+-----------+-----------+-----------+";
