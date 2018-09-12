1442b6d955b5e380921c97726d028554d8d1e233
richard davy <richard_davy@hotmail.com>
2018-09-12 09:19:17 +0200

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sudoku game = new Sudoku("103020600900305001001806400" +
+                                        "008102900700000008006708200" +
+                                        "002609500800203009005010300");
+
+            //game.Solve();
+            //Console.WriteLine(game.BoardAsText);   
+            
+        }
+    }
+}
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Sudoku
+    {
+        const int WidthOfBoard = 9;
+
+        public string BoardAsText { get; set; }
+
+
+        public Sudoku(string initArragement)
+        {
+            BoardAsText = FormatString(initArragement);
+            Console.WriteLine(BoardAsText);
+        }
+
+        public string FormatString (string initArragement)
+        {
+            char[,] matrix = new char[9, 9];
+            string formatedMatrix = "+------+------+------+\n";
+
+            for (int i = 0; i < WidthOfBoard; i++)
+            {
+                for (int j = 0; j < WidthOfBoard; j++)
+                {
+                    matrix[i, j] = initArragement[j];
+                    formatedMatrix += String.Format("| {0} ", matrix[i, j]);
+                }
+                formatedMatrix += "|\n";
+                if ((i+1) % 3 == 0)
+                {
+                    formatedMatrix += "+------+------+------+\n";
+                }
+            }
+            formatedMatrix += "+------+------+------+";
+
+            return formatedMatrix;
+        }
+
+
+
+        public void Solve()
+        {
+
+        }
+
+
+    }
+}
